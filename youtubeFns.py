722b888f5ce41e809f7becaef0f628cede31ada8
kyrie0126 <kylewong00@gmail.com>
2023-10-25 15:38:36 -0700

    created and implemented AnalyseChannel class

--- youtubeFns.py
+++ youtubeFns.py
@@ -11,143 +11,161 @@ load_dotenv()
 api_key = os.getenv('YT_API_KEY')
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-
-def retrieve_channel_stats(id):
-    """
-    Return channel statistics as dictionary
+class AnalyzeChannel():
     
-    Params:
-    
-    username: (str) channel username
-    """
-
-    req = youtube.channels().list(
-        part=['statistics', 'snippet'],
-        id = id
-    ).execute()
-    
-    stats = {
-        'username': req['items'][0]['snippet']['title'],
-        'channel_id': req['items'][0]['id'],
-        'channel_views': req['items'][0]['statistics']['viewCount'],
-        'subscriber': req['items'][0]['statistics']['subscriberCount'],
-        'video_count': req['items'][0]['statistics']['videoCount']
-    }
-    
-    return stats
+        
+    def __init__(self, api_key, url) -> None:
+        self.__api_key = api_key
+        self.__youtube = build('youtube', 'v3', developerKey=self.__api_key)
+         
+        self.channel_id = self.retrieve_channel_id(url)
+        self.channel_stats = self.retrieve_channel_stats()
+        self.channel_username = self.channel_stats['username']
+        self.channel_view_count = int(self.channel_stats['channel_views'])
+        self.channel_subscriber_count = int(self.channel_stats['subscriber'])
+        self.channel_video_count = int(self.channel_stats['video_count'])
 
 
-def retrieve_channel_id(url):
-    """
-    Return channel id as string
-    
-    Params:
-    
-    url: (str) url of any video posted by channel
-    """
+    def retrieve_channel_id(self, url):
+            """
+            Return channel id as string
+            
+            Params:
+            
+            url: (str) url of any video posted by channel
+            """
 
-    vid_id = url.split('=')[-1]
+            vid_id = url.split('=')[-1]
 
-    req = youtube.videos().list(
-        part='snippet',
-        id=vid_id).execute()
-    
-    channel_id = req['items'][0]['snippet']['channelId']
-    
-    return channel_id
+            req = self.__youtube.videos().list(
+                part='snippet',
+                id=vid_id).execute()
+            
+            channel_id = req['items'][0]['snippet']['channelId']
+            
+            return channel_id
 
 
-def retrieve_channel_videos(id):
-    """Retrieve all videos from a channel, return as dataframe
-    
-    Params (require at least 1):\n
-    channel_id: (str) unique channel id assigned by youtube \n
-    username: (str) channel username seen by viewers
-    """
-    
-    # retrieve uploads playlist id
-    channel_info = youtube.channels().list(
-        id=id,
-        part='contentDetails'
-    ).execute()
-    
-    uploads_id = channel_info['items'][0]['contentDetails']['relatedPlaylists']['uploads']
-    
-    
-    # retrieve video ids
-    vid_ids = []
-    next_page_token = None
-    
-    while True:
-        res = youtube.playlistItems().list(
-            playlistId=uploads_id,
-            part=['snippet', 'contentDetails'],
-            maxResults=50,
-            pageToken=next_page_token
+    def retrieve_channel_stats(self):
+        """
+        Return channel statistics as dictionary
+        
+        Params:
+        
+        username: (str) channel username
+        """
+
+        req = self.__youtube.channels().list(
+            part=['statistics', 'snippet'],
+            id = self.channel_id
         ).execute()
-        for video in res['items']:
-            vid_ids.append(video['contentDetails']['videoId'])
+        
+        stats = {
+            'username': req['items'][0]['snippet']['title'],
+            'channel_id': req['items'][0]['id'],
+            'channel_views': req['items'][0]['statistics']['viewCount'],
+            'subscriber': req['items'][0]['statistics']['subscriberCount'],
+            'video_count': req['items'][0]['statistics']['videoCount']
+        }
+        
+        return stats
+
+
+    def retrieve_channel_videos(self):
+        """Retrieve all videos from a channel, return as dataframe
+        
+        Params (require at least 1):\n
+        channel_id: (str) unique channel id assigned by youtube \n
+        username: (str) channel username seen by viewers
+        """
+        
+        # retrieve uploads playlist id
+        channel_info = self.__youtube.channels().list(
+            id=self.channel_id,
+            part='contentDetails'
+        ).execute()
+        
+        uploads_id = channel_info['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+        
+        
+        # retrieve video ids
+        vid_ids = []
+        next_page_token = None
+        
+        while True:
+            res = self.__youtube.playlistItems().list(
+                playlistId=uploads_id,
+                part=['snippet', 'contentDetails'],
+                maxResults=50,
+                pageToken=next_page_token
+            ).execute()
+            for video in res['items']:
+                vid_ids.append(video['contentDetails']['videoId'])
+                
+            next_page_token = res.get('nextPageToken')
             
-        next_page_token = res.get('nextPageToken')
-        
-        if next_page_token is None:
-            break
-        
-    
-    # retrieve video data
-    vid50 = [vid_ids[i * 50:(i + 1) * 50] for i in range((len(vid_ids) + 49) // 50 )]
-    video_data = []
-
-    for i in vid50:
-        req = youtube.videos().list(
-        part=['snippet', 'statistics', 'contentDetails'],
-        id=i).execute()
-        
-        for video in req['items']:
-            vid_dict = {
-                'id': video['id'],
-                'title': video['snippet']['title'],
-                'publishTime': video['snippet']['publishedAt'],
-                'duration': video['contentDetails']['duration'],
-                'views': video['statistics']['viewCount'],
-                'likes': video['statistics']['likeCount'],
-                'comments': video['statistics']['commentCount']
-            }
-            video_data.append(vid_dict)
+            if next_page_token is None:
+                break
             
-    df = pd.DataFrame.from_records(video_data)
-    
-    # clean datetime columns
-    df['publishTime'] = pd.to_datetime(df['publishTime'])
-    df['day_name'] = df['publishTime'].dt.day_name()
+        
+        # retrieve video data
+        vid50 = [vid_ids[i * 50:(i + 1) * 50] for i in range((len(vid_ids) + 49) // 50 )]
+        video_data = []
 
-    # Define custom weekday order
-    weekdays_order = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    weekday_cat_dtype = pd.api.types.CategoricalDtype(categories=weekdays_order, ordered=True)
-    df['weekday_name'] = df['day_name'].astype(weekday_cat_dtype)
+        for i in vid50:
+            req = self.__youtube.videos().list(
+            part=['snippet', 'statistics', 'contentDetails'],
+            id=i).execute()
+            
+            for video in req['items']:
+                vid_dict = {
+                    'id': video['id'],
+                    'title': video['snippet']['title'],
+                    'publishTime': video['snippet']['publishedAt'],
+                    'duration': video['contentDetails']['duration'],
+                    'views': video['statistics']['viewCount'],
+                    'likes': video['statistics']['likeCount'],
+                    'comments': video['statistics']['commentCount']
+                }
+                video_data.append(vid_dict)
+                
+        df = pd.DataFrame.from_records(video_data)
+        
+        # clean datetime columns
+        df['publishTime'] = pd.to_datetime(df['publishTime'])
+        df['day_name'] = df['publishTime'].dt.day_name()
 
-    # clean ISO encoded duration column
-    df['duration'] = pd.to_timedelta(df['duration'])
-    df['duration_min'] = round(df['duration'].dt.components.minutes + df['duration'].dt.components.seconds / 60, 2)
-    
-    # indicate full video or YT short
-    df['video_type'] = np.where(df['duration_min'] > 1, 'standard', 'short')
-    
-    # convert to numeric
-    df['views'] = df['views'].astype('int')
-    df['likes'] = df['likes'].astype('int')
-    df['comments'] = df['comments'].astype('int')
+        # Define custom weekday order
+        weekdays_order = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        weekday_cat_dtype = pd.api.types.CategoricalDtype(categories=weekdays_order, ordered=True)
+        df['weekday_name'] = df['day_name'].astype(weekday_cat_dtype)
 
-    return df
-    
-    
+        # clean ISO encoded duration column
+        df['duration'] = pd.to_timedelta(df['duration'])
+        df['duration_min'] = round(df['duration'].dt.components.minutes + df['duration'].dt.components.seconds / 60, 2)
+        
+        # indicate full video or YT short
+        df['video_type'] = np.where(df['duration_min'] > 1, 'standard', 'short')
+        
+        # convert to numeric
+        df['views'] = df['views'].astype('int')
+        df['likes'] = df['likes'].astype('int')
+        df['comments'] = df['comments'].astype('int')
+
+        return df
+        
+        
 def calculate_lof(df, variable: str):
     """
-    Returns dataframe with Local Outlier Factor (LOF) algorithm applied to variable of choice
+    Applies unsupervised local outlier factor (LOF) algorithm to variable of interest to classify 
+    video performance as hit or normal
     
-    Params:\n
-    df: (pd.DataFrame)\n
-    variable: (str) variable of interest
+    Args:
+        df (pd.DataFrame): dataframe from `retrieve_channel_videos` function in `AnalyzeChannel()` class
+        variable (str): variable of interest
+    
+    Returns:
+        pd.DataFrame: initial dataframe with two extra columns containing LOF result 
     """
     anomaly_inputs = [variable]
         
@@ -168,12 +186,27 @@ def calculate_lof(df, variable: str):
     return pd.concat([df_video, df_short], axis=0)
     
     
-def visualize_performance(CHANNEL_USERNAME, df, variable: str, vid_type: str):
+    
+    
+    
+def visualize_performance(username, df, variable: str, vid_type: str):
+    """
+    Creates interactive scatterplot of video performance comparing normal videos to hit videos
+    
+    Args:
+        username (str): name of YouTube channel
+        df (DataFrame): output from `calculate_lof` function
+        variable (str): column of interest from df
+        vid_type (str): type of YT video with 2 choices: 'standard' or 'short'
+    
+    Returns:
+        plotly scatterplot
+    """
     
     if vid_type=='standard':
-        graph_title = f"{CHANNEL_USERNAME}'s Youtube Video Performance ({variable})"
+        graph_title = f"{username}'s Youtube Video Performance ({variable})"
     else:
-        graph_title = f"{CHANNEL_USERNAME}'s Youtube Shorts Performance ({variable})"
+        graph_title = f"{username}'s Youtube Shorts Performance ({variable})"
     
     df_full = df[df['video_type']==vid_type].copy()
     
